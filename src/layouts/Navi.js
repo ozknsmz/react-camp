df99f6c25989ce9abc1f209a2233781b8b476a7d
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 14:16:38 +0300

    created functions sign in and out isAuthenticated

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import { Button, Dropdown, Menu } from "semantic-ui-react";
 import { Container, Divider, Header } from "semantic-ui-react";
 import CardSummary from "./CardSummary";
 import SignedIn from "./SignedIn";
-import SignedOut from "./SignedOut"
+import SignedOut from "./SignedOut";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  function handleSignOut() {
+    setIsAuthenticated(false);
+  }
+
+  function handleSignIn() {
+    setIsAuthenticated(true);
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -14,8 +25,11 @@ export default function Navi() {
           <Menu.Item name="messages" />
           <Menu.Menu position="right">
             <CardSummary />
-            <SignedOut/>
-            <SignedIn/>
+            {isAuthenticated ? (
+              <SignedIn signOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
