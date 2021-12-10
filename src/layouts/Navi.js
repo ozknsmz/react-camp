b184b03aa44b2a9bb5a272939312deaeff933d9a
ozknsmz <ozknsmz@hotmail.com>
2021-12-11 00:50:58 +0300

    added nav link

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Dropdown, Menu } from "semantic-ui-react";
 import { Container, Divider, Header } from "semantic-ui-react";
 import CardSummary from "./CardSummary";
 import SignedIn from "./SignedIn";
 import SignedOut from "./SignedOut";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Navi() {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
+  let history = useNavigate();
+
+
   function handleSignOut() {
     setIsAuthenticated(false);
+    history('/');
   }
 
   function handleSignIn() {
     setIsAuthenticated(true);
   }
+  
 
   return (
     <div>
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item name="home" />
+          <Menu.Item name="home" as={NavLink} to="/" />
           <Menu.Item name="messages" />
           <Menu.Menu position="right">
             <CardSummary />
