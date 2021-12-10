51d7c87e41c380685478c729e42b0d0d87950dbe
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 23:13:47 +0300

    used routers

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,18 +1,28 @@
 import React from "react";
 import Categories from "./Categories";
-import { Grid } from 'semantic-ui-react'
+import { Grid } from "semantic-ui-react";
 import ProductList from "../Pages/ProductList";
+import { Route, Routes } from "react-router-dom";
+import ProductDetails from "../Pages/ProductDetails";
+import CartDetail from "../Pages/CartDetail";
 
 export default function Dashboard() {
   return (
     <div>
       <Grid>
-        <Grid.Column width={4}>
-          <Categories />
-        </Grid.Column>
-        <Grid.Column width={12}>
-          <ProductList />
-        </Grid.Column>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Categories />
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Routes>
+            <Route path="/" element={<ProductList/>} />
+            <Route path="/Products" element={<ProductList/>} />
+            <Route path="/products/:id" element={<ProductDetails/>} />
+            <Route path="/cart" element={<CartDetail/>} />
+            </Routes>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </div>
   );
