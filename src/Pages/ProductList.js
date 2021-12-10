f0c832eabd986d41e5727b4883fafdf4738c1ef1
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 11:36:37 +0300

    useState and esuEffect to get data from backend

--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -1,36 +1,41 @@
-import React from "react";
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React, { useState, useEffect } from "react";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import ProductService from "../services/productService";
 
 export default function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    // component yüklendiğinde yapılmasını istediğin işlemi bu kısma yazıyorsun
+    let productService = new ProductService()
+    productService.getProduct().then(result=>setProducts(result.data.data))
+
+    //then() başarılı olursa manasında
+  })
+
   return (
     <div>
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
+            <Table.HeaderCell>Birim Fiyatı</Table.HeaderCell>
+            <Table.HeaderCell>Stok Adedi</Table.HeaderCell>
+            <Table.HeaderCell>Açıklama</Table.HeaderCell>
+            <Table.HeaderCell>Kategori</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Label ribbon>First</Label>
-            </Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
+          {products.map((product) => (
+            <Table.Row key={product.id}>
+              <Table.Cell>{product.productName}</Table.Cell>
+              <Table.Cell>{product.unitPrice}</Table.Cell>
+              <Table.Cell>{product.unitsInStock}</Table.Cell>
+              <Table.Cell>{product.quantityPerUnit}</Table.Cell>
+              <Table.Cell>{product.category.categoryName}</Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
 
         <Table.Footer>
