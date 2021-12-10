26e6bba7490877d0255beb2984fc7a48a7b980fc
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 14:15:18 +0300

    to sign in system, used props with onclick

--- src/layouts/SignedIn.js
+++ src/layouts/SignedIn.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Dropdown, Image, Menu } from "semantic-ui-react";
 
-export default function SignedIn() {
+export default function SignedIn(props) {
   return (
     <div>
       <Menu.Item>
@@ -13,7 +13,7 @@ export default function SignedIn() {
         <Dropdown pointing="top-left" text="Özkan">
           <Dropdown.Menu>
             <Dropdown.Item text="Bilgilerim" icon="info"></Dropdown.Item>
-            <Dropdown.Item text="Çıkış Yap" icon="sign-out"></Dropdown.Item>
+            <Dropdown.Item onClick={props.signOut} text="Çıkış Yap" icon="sign-out"></Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Menu.Item>
