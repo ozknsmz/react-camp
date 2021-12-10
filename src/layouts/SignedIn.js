4af10432fe1d69beff155d2e7e2d9dfc5f72b2ce
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 13:42:31 +0300

    sign in js added

---
+++ src/layouts/SignedIn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Dropdown, Image, Menu } from "semantic-ui-react";
+
+export default function SignedIn() {
+  return (
+    <div>
+      <Menu.Item>
+        <Image
+          avatar
+          spaced="right"
+          src="https://avatars.githubusercontent.com/u/45689757?v=4"
+        />
+        <Dropdown pointing="top-left" text="Özkan">
+          <Dropdown.Menu>
+            <Dropdown.Item text="Bilgilerim" icon="info"></Dropdown.Item>
+            <Dropdown.Item text="Çıkış Yap" icon="sign-out"></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+    </div>
+  );
+}
