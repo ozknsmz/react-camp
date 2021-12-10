71751d1b6f209937db4af422834b607be3dfb4ac
ozknsmz <ozknsmz@hotmail.com>
2021-12-10 14:15:26 +0300

    to sign out system, used props with onclick

--- src/layouts/SignedOut.js
+++ src/layouts/SignedOut.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Button, Menu } from "semantic-ui-react";
 
 
-export default function SignedOut() {
+export default function SignedOut(props) {
   return (
     <div>
       <Menu.Item>
-      <Button primary>Giriş Yap</Button>
+      <Button onClick={props.signIn} primary>Giriş Yap</Button>
       <Button className="kayıt-ol" primary>Kayıt Ol</Button>
       </Menu.Item>
     </div>
